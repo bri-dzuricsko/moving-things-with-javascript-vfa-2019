1d01060cb05c5a4c6e764ff09a584b363398ad5e
Brianna Dzuricsko <bri.dzuricsko@gmail.com>
2019-06-12 00:13:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var dodger = document.getElementById('dodger')
 dodger.style.backgroundColor = '#FF69B4'
 dodger.style.left // "180px"
 dodger.style.bottom // "0px"
+
+document.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})

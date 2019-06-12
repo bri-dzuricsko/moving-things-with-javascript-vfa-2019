c566ea7c9043f48c080c53dd8f1e8f7e3fcd3476
Brianna Dzuricsko <bri.dzuricsko@gmail.com>
2019-06-12 00:14:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ document.addEventListener('keydown', function(e) {
     dodger.style.left = `${left - 1}px`
   }
 })
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
